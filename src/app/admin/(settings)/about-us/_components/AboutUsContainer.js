8be2d68971fcf40c07749561d05547b21374ef67
Moazzem-Bhuiyan <tiyonbhuiyan.201@gmail.com>
2025-07-02 22:52:01 -0700

    update

--- src/app/admin/(settings)/about-us/_components/AboutUsContainer.js
+++ src/app/admin/(settings)/about-us/_components/AboutUsContainer.js
@@ -8,11 +8,11 @@ import { Edit } from 'lucide-react';
 export default function AboutUsContainer() {
   return (
     <section>
-      <h3 className="text-2xl font-semibold mb-6">About Us</h3>
+      <h3 className="text-2xl font-semibold mb-6">About Us </h3>
 
       <FormWrapper>
         <UTextEditor
-          name="privacyPolicy"
+          name="aboutUs"
           placeholder="Note: Enter details about the website here. (e.g How and why did you come up with the idea? etc)"
         />
 
